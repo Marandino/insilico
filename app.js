fe855a08e64f7debcbfe654d451f30c24a7e0843
Chiyen Venegas <chiy100196@gmail.com>
2020-06-19 13:55:11 +0100

    add passport related dependencies, add post route to register and config it to create a new item within the collection in mongoatlas

--- app.js
+++ app.js
@@ -1,22 +1,45 @@
 var express = require("express"),
     app = express(),
-    // mongoose = require('mongoose'),
-    // methodOverride = require('method-override'),
-    // uri = 'mongodb+srv://marandino:herediano@cluster0-deig9.gcp.mongodb.net/insilico?retryWrites=true&w=majority',
+    ///MANAGING DATABASES
+    mongoose = require('mongoose'),
+    ///BODY PARSER (PARSE FORMS)
     bodyParser = require('body-parser'),
+    ///PASSPORT DEPENDENCIES (AUTHENTICATION)
+    User = require("./models/users"),
+    passport = require("passport"),
+    LocalStrategy = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose"),
+    //// PORT
     PORT = process.env.PORT || 5000;
+////CONNECT TO DATABASE
+const uri = "mongodb+srv://marandino:herediano@cluster0-deig9.gcp.mongodb.net/?retryWrites=true&w=majority";
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect(uri, {
+    useNewUrlParser: true
+});
 
-// mongoose.set('useUnifiedTopology', true);
-// mongoose.connect(uri, {
-//     useNewUrlParser: true
-// });
-
+////=======
+//INIT DEPENDENCIES
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.use(require("express-session")({
+    secret: "encoding passwords2",
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+
+///SET VIEW ENGINE AND PUBLIC DIR
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-// app.use(methodOverride("_method"));
+
+///// PASSPORT INITIALIZATION
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+//=========
 
 app.get("/", function (req, res) {
     res.render("index");
@@ -27,6 +50,25 @@ app.get("/", function (req, res) {
 app.get("/register", (req, res) => {
     res.render("register")
 })
+//POST ROUTE
+app.post("/register", (req, res) => {
+    //user REGISTRATION
+    User.register(new User({
+        username: req.body.username,
+        email: req.body.email
+    }), req.body.password, function (err, user) {
+        if (err) {
+            console.log(err)
+            return res.render("register");
+        }
+
+        //this part logs in the user after registering
+        passport.authenticate("local")(req, res, function () {
+            res.redirect("/");
+        })
+    })
+})
+/////======= END OF REGISTRATION ========
 
 //LOG IN
 app.get("/login", (req, res) => {
