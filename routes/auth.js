4b5e95a8f55af296a385d860c107997a622926cd
Chiyen Venegas <chiy100196@gmail.com>
2020-07-30 16:53:02 +0100

    add oauth to nodemailer

--- routes/auth.js
+++ routes/auth.js
@@ -8,21 +8,35 @@ const express = require("express"),
   User = require("../models/users"),
   router = express.Router();
 ////EMAILING VARIABLES
+//GMAIL 0Auth
+const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
+
+const oauth2Client = new OAuth2(
+  process.env.OAUTH_CLIENT_ID, // ClientID
+  process.env.OAUTH_CLIENT_SECRET, // Client Secret
+  "https://developers.google.com/oauthplayground" // Redirect URL
+);
+
+oauth2Client.setCredentials({
+  refresh_token: process.env.REFRESH_TOKEN,
+});
+const accessToken = oauth2Client.getAccessToken();
+///END OF GOOGLE OAUTH
+///NODEMAILER
 const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true, // true for 465, false for other ports
+  service: "gmail",
   auth: {
-    user: "service@insilicotrading.info", // generated ethereal user
-    pass: process.env.MARANDINO_PASSWORD, // generated ethereal password
+    type: "OAuth2",
+    user: "service@insilicotrading.info",
+    clientId: process.env.OAUTH_CLIENT_ID,
+    clientSecret: process.env.OAUTH_CLIENT_SECRET,
+    refreshToken: process.env.REFRESH_TOKEN,
+    accessToken: accessToken,
   },
 });
-
-// const sgMail = require("@sendgrid/mail"),
-//   { update } = require("../models/lessons");
-// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
+////====
 //////<
 ///USER AUTHENTICATION
 //REGISTER
--- routes/contact.js
+++ routes/contact.js
@@ -1,18 +1,35 @@
 ///express router
 const express = require("express"),
   router = express.Router();
-// const sgMail = require("@sendgrid/mail");
-// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+///GMAIL 0Auth
+const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
+
+const oauth2Client = new OAuth2(
+  process.env.OAUTH_CLIENT_ID, // ClientID
+  process.env.OAUTH_CLIENT_SECRET, // Client Secret
+  "https://developers.google.com/oauthplayground" // Redirect URL
+);
+
+oauth2Client.setCredentials({
+  refresh_token: process.env.REFRESH_TOKEN,
+});
+const accessToken = oauth2Client.getAccessToken();
+///END OF GOOGLE OAUTH
+///NODEMAILER
 const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true, // true for 465, false for other ports
+  service: "gmail",
   auth: {
-    user: "service@insilicotrading.info", // generated ethereal user
-    pass: process.env.MARANDINO_PASSWORD, // generated ethereal password
+    type: "OAuth2",
+    user: "service@insilicotrading.info",
+    clientId: process.env.OAUTH_CLIENT_ID,
+    clientSecret: process.env.OAUTH_CLIENT_SECRET,
+    refreshToken: process.env.REFRESH_TOKEN,
+    accessToken: accessToken,
   },
 });
+////====
 
 /// CONTACT FORM
 router.get("/contact", function (req, res) {
